7fcb264e7ca79326dfbd0cf7b6cbda2cb5581716
Antonio MalvaGomes <amalvagomes@manh.com>
2016-08-19 11:57:32 -0400

    Adds first attempt at sleuth support without an auto-configuration or starter.

--- src/main/java/net/lariverosc/jesquespring/SpringJobFactory.java
+++ src/main/java/net/lariverosc/jesquespring/SpringJobFactory.java
@@ -4,6 +4,8 @@ import net.greghaines.jesque.Job;
 import net.greghaines.jesque.worker.JobFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -12,9 +14,20 @@ public class SpringJobFactory implements JobFactory, ApplicationContextAware {
     private final Logger logger = LoggerFactory.getLogger(SpringJobFactory.class);
 
     private ApplicationContext applicationContext;
+    
+    private Tracer tracer;
 
     @Override
     public Object materializeJob(Job job) throws Exception {
+        
+        if(null == tracer) {
+            try {
+                tracer = applicationContext.getBean(Tracer.class);
+            } catch(NoSuchBeanDefinitionException e) {
+                logger.debug("Tracer is not present in context.  Skipping sleuth instrumentation.");
+            }
+        }
+        
         Runnable runnableJob = null;
         if (applicationContext.containsBeanDefinition(job.getClassName())) {//Lookup by bean Id
             runnableJob = (Runnable) applicationContext.getBean(job.getClassName(), job.getArgs());
@@ -34,6 +47,13 @@ public class SpringJobFactory implements JobFactory, ApplicationContextAware {
                 throw new Exception("Not bean Id or class definition found " + job.getClassName());
             }
         }
+        
+        if(null != tracer) {
+            logger.debug("Tracer was not null.  Wrapping...");
+            runnableJob = tracer.wrap(runnableJob);
+            logger.debug("Runnable is now {}.", runnableJob);
+        }
+        
         return runnableJob;
     }
 
